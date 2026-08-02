3a037be9c04ef0b924c7d4030345c48bd440cfc3
rajrai <rajrai913197@gmail.com>
2026-08-03 02:25:07 +0530

    do works on footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,55 @@
+import { useNavigate } from 'react-router-dom'
+
 const Footer = ()=>{
+  let navigate =   useNavigate()
     return(
         <>
-         <footer>
-            
+         <footer className='main-footer'>
+            <section className='contact-links'>
+              <h3>DOTCOME</h3>
+             <h4>STAY UP TO DATE</h4>
+             <div className='links-connect'>
+                <a href="https://www.instagram.com/raa.j_kalar?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://github.com/rajrai913197-png"><i class="fa-brands fa-github"></i></a>
+                <a href="https://x.com/paradox_raz"><i class="fa-brands fa-x-twitter"></i></a>
+                <a href="https://www.facebook.com/profile.php?id=100037591242603"><i class="fa-brands fa-facebook"></i></a>
+             </div>
+            <button onClick={()=> navigate("/")}>BACK TO HOME <i class="fa-solid fa-rotate-left"></i></button>
+            </section>
+            <section className='imp-things'>
+             <a href="">contact us</a>
+            <a href="">promotons and sele</a>
+             <a href="">my account</a>
+            <a href="">exchange and return</a>
+            <a href="">privacy and policy</a>
+             <a href="">term and conditions</a>
+             <a href="">shoes</a>
+             <a href="">running shoes</a>
+              <a href="">sitemap</a>
+            </section>
+            <section className='site-rules'>
+             <a href="">FAQ</a>
+            <a href="">warranty claims</a>
+             <a href="">track order</a>
+            <a href="">shoes care</a>
+            <a href="">tech glossary</a>
+             <a href="">Initiate Return / Exchange</a>
+             <a href="">Sneakers</a>
+             <a href="">Nitro</a>
+              <a href="">Cookie Settings</a>
+            </section>
+             <section className='about-page'>
+                <h4>about</h4>
+             <a href="">Company</a>
+            <a href="">What is cartify</a>
+             <a href="">Corporate News</a>
+            <a href="">Press Center</a>
+            <a href="">Investors</a>
+             <a href="">Sustainability</a>
+             <a href="">Careers</a>
+             <a href="">Store Locator</a>
+              <a href="">CARTYFY articles</a>
+            </section>
          </footer>
         </>
     )
