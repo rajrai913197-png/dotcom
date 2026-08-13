0db53b3c22475570bbf5d42f0f2196ecb22f6f42
rajrai <rajrai913197@gmail.com>
2026-08-14 00:13:12 +0530

    work on slidders

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -24,16 +24,15 @@ const Navbar = ()=>{
          <i class="fa-solid fa-bars" id='burger' onClick={()=> setMenu(!menu)}></i>
         </nav>
         <div className={`implinks ${menu ? "open" : ""}`}>
-  <button onClick={()=> setMenu(false) } id='wrong-icon'><i class="fa-solid fa-circle-xmark"></i></button>
-  <Link to="/">HOME</Link>
-  <Link to="/products">PRODUCT</Link>
-  <Link to="/men">MEN</Link>
-  <Link to="/women">WOMEN</Link>
-  <Link to="/kids">KIDS</Link>
-  <Link to="/cart"><div className='btn-links'>CART<i class="fa-solid fa-arrow-right-long"></i></div></Link>
-  <Link to="/wishlist"><div className='btn-links'>WISHLIST<i class="fa-solid fa-arrow-right-long"></i></div></Link>
+  <Link to="/" onClick={()=>setMenu(false)}>HOME</Link>
+  <Link to="/products"  onClick={()=>setMenu(false)}>PRODUCT</Link>
+  <Link to="/men"  onClick={()=>setMenu(false)}>MEN</Link>
+  <Link to="/women"  onClick={()=>setMenu(false)}>WOMEN</Link>
+  <Link to="/kids"  onClick={()=>setMenu(false)}>KIDS</Link>
+  <Link to="/cart"  onClick={()=>setMenu(false)}><div className='btn-links'>CART<i class="fa-solid fa-arrow-right-long"></i></div></Link>
+  <Link to="/wishlist"  onClick={()=>setMenu(false)}><div className='btn-links'>WISHLIST<i class="fa-solid fa-arrow-right-long"></i></div></Link>
   <Link to="/login" id="login">
-   <div className='user-login'>LOGIN<i class="fa-solid fa-arrow-down-long"></i></div> 
+   <div className='user-login'  onClick={()=>setMenu(false)}>LOGIN<i class="fa-solid fa-arrow-down-long"></i></div> 
   </Link>
 </div>
         </>
