bcffb000e5455e2c8346a3e7d853a6d2175a1329
rajrai <rajrai913197@gmail.com>
2026-08-22 19:11:57 +0530

    make collection divs

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,38 @@
 import EmblaCarousel from "./EmblaCarousel"
-
+import male1 from "../assets/posters/maleposter.jpg"
 const Home =()=>{
 
     return(
         <>
        <div>
       <EmblaCarousel  />
-    </div>
+       </div>
+      <div className="collection-div">
+        <h1 style={{textAlign:"center"}} className="our-text">OUR COLLECTION</h1>
+        <div className="collection-posters-div">
+        <div className="collections-poster-div">
+             <img src={male1} alt="" className="img-collection" />
+            <div className="info-collection-div">
+                <h1>MALE</h1>
+              <button>EXPLORE<i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+        </div>
+         <div className="collections-poster-div">
+             <img src={male1} alt="" className="img-collection"  />
+            <div className="info-collection-div">
+                <h1>WOMEN</h1>
+              <button>EXPLORE<i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+        </div>
+         <div className="collections-poster-div">
+             <img src={male1}alt="" className="img-collection"  />
+            <div className="info-collection-div">
+                <h1>KIDS</h1>
+              <button>EXPLORE<i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+        </div>
+        </div>
+      </div>
         </>
     )
 }
